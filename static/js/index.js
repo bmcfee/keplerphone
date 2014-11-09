dd859dd3a88be1ef4a733a242af346e2afe0ccc1
Brian McFee <brian.mcfee@nyu.edu>
2014-11-09 00:26:17 -0500

    checking in cruft

---
+++ static/js/index.js
@@ -0,0 +1,27 @@
+// nothing here yet
+
+$(document).ready(function() {
+//     search_graph($('#seed_id').val(), $('#depth').val());
+    // Query for the collections list
+//     $.ajax({ url: '/collections', dataType: 'json'}).done(update_collections);
+
+    // Set the default and update the pager
+//     var collection_id   = parseInt($('#collection_id').val());
+//     get_collection(collection_id, 0, 18);
+
+//     $('#track-min').change(function(eventObject) {
+//         var offset = parseInt($(this).val());
+
+//         if (offset < 1 || offset >=
+//             parseInt($('#track_count').val())) {
+
+//             $('#track-form').addClass('has-error');
+//             return;
+//         }
+//         $('#track-form').removeClass('has-error');
+
+//         var collection_id   = parseInt($('#collection_id').val());
+
+//         get_collection(collection_id, offset-1, 18);
+//     });
+});
